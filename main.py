ce3a3deed5dbf1dbfa613888f5d69710c15e6d50
Folajimi Fabusuyi <folajimifabusuyi@gmail.com>
2024-09-19 04:34:57 +0000

    Add chatbot menu and functionality

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+def print_menu():
+    global username
+    print(f"How may I help you {username}:")
+    print("\t1. -- Placeholder Option --")
+    print("\t2. -- Placeholder Option --")
+    print("\t3. -- Placeholder Option --")
+    print("\t4. -- End Conversation --")
+
+    choice = input("Enter the number associated with your choice: ")
+    while not choice.isdigit() or (int(choice) > 4 or int(choice) < 1):
+        print("\tPlease input a valid number")
+        choice = input("\nEnter the number associated with your choice: ")
+
+    return int(choice)
+
+
+# Welcome Sequence
 print("Welcome to the Elite 101 Chatbot, I am at your service.")
 print("Before we begin, some basic information is needed.")
 
+
+# User info gathering process.
 confirmation = False
 while not confirmation:
     username = input("\n\tEnter your name: ")
@@ -21,3 +40,13 @@ while not confirmation:
             break
         else:
             print("Invalid option")
+
+# Menu
+while True:
+    choice = print_menu()
+    if choice == 4:
+        print(f"Signing out user: {username}")
+        print("Have a great day!")
+        break
+    else:
+        continue
